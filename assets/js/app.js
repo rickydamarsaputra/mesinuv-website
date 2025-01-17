b5a06ff027985fe8439cb757fa2b1cef2e3670ea
rickydamarsaputra <retrocode.rc@gmail.com>
2025-01-17 14:42:00 +0700

    update: section cta sales

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,6 +28,40 @@ createApp({
         link_product: 'https://vicentra.co.id/product/mesin/digital-printing/uv/neu-jet-mpuvrh1804',
       }
     ]);
+    const salesPeople = ref([
+      {
+        id: 1,
+        name: 'Layanan 24 Jam',
+        slug: 'layanan_24_jam',
+        phone: '6281232548368',
+        photo: 'https://vicentra.co.id/storage/sales-people/vicentra-24-jam.webp',
+        additional_sentence: 'Ada pertanyaan atau ingin memesan produk ini?'
+      },
+      {
+        id: 2,
+        name: 'Yaya',
+        slug: 'yaya',
+        phone: '6285175005535',
+        photo: 'https://vicentra.co.id/storage/sales-people/sales-yaya.webp',
+        additional_sentence: 'Support 24 jam'
+      },
+      {
+        id: 3,
+        name: 'Alda',
+        slug: 'alda',
+        phone: '6285183171200',
+        photo: 'https://vicentra.co.id/storage/sales-people/sales-alda.webp',
+        additional_sentence: 'Ada pertanyaan atau ingin memesan produk ini?'
+      },
+      {
+        id: 4,
+        name: 'Ella',
+        slug: 'ella',
+        phone: '6285733399974',
+        photo: 'https://vicentra.co.id/storage/sales-people/sales-ella.webp',
+        additional_sentence: 'Ada pertanyaan atau ingin memesan produk ini?'
+      },
+    ]);
     const isMenuShow = ref(false);
 
     const toggleMenu = () => {
@@ -36,6 +70,7 @@ createApp({
 
     return {
       products,
+      salesPeople,
       isMenuShow,
       toggleMenu
     }
