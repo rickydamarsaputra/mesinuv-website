d0fea26fd60c3520fbd3fb30908f5e15a182901e
rickydamarsaputra <retrocode.rc@gmail.com>
2025-01-23 14:06:00 +0700

    update:cta, hanya tampilkan wa pak fifik.

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,30 +37,30 @@ createApp({
         photo: 'https://vicentra.co.id/storage/sales-people/vicentra-24-jam.webp',
         additional_sentence: 'Ada pertanyaan atau ingin memesan produk ini?'
       },
-      {
-        id: 2,
-        name: 'Yaya',
-        slug: 'yaya',
-        phone: '6285175005535',
-        photo: 'https://vicentra.co.id/storage/sales-people/sales-yaya.webp',
-        additional_sentence: 'Support 24 jam'
-      },
-      {
-        id: 3,
-        name: 'Alda',
-        slug: 'alda',
-        phone: '6285183171200',
-        photo: 'https://vicentra.co.id/storage/sales-people/sales-alda.webp',
-        additional_sentence: 'Ada pertanyaan atau ingin memesan produk ini?'
-      },
-      {
-        id: 4,
-        name: 'Ella',
-        slug: 'ella',
-        phone: '6285733399974',
-        photo: 'https://vicentra.co.id/storage/sales-people/sales-ella.webp',
-        additional_sentence: 'Ada pertanyaan atau ingin memesan produk ini?'
-      },
+      // {
+      //   id: 2,
+      //   name: 'Yaya',
+      //   slug: 'yaya',
+      //   phone: '6285175005535',
+      //   photo: 'https://vicentra.co.id/storage/sales-people/sales-yaya.webp',
+      //   additional_sentence: 'Support 24 jam'
+      // },
+      // {
+      //   id: 3,
+      //   name: 'Alda',
+      //   slug: 'alda',
+      //   phone: '6285183171200',
+      //   photo: 'https://vicentra.co.id/storage/sales-people/sales-alda.webp',
+      //   additional_sentence: 'Ada pertanyaan atau ingin memesan produk ini?'
+      // },
+      // {
+      //   id: 4,
+      //   name: 'Ella',
+      //   slug: 'ella',
+      //   phone: '6285733399974',
+      //   photo: 'https://vicentra.co.id/storage/sales-people/sales-ella.webp',
+      //   additional_sentence: 'Ada pertanyaan atau ingin memesan produk ini?'
+      // },
     ]);
     const isMenuShow = ref(false);
 
