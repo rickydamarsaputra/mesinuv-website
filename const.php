5e45a4c8711d98e866f978749326fff45849fe38
rickydamarsaputra <retrocode.rc@gmail.com>
2025-02-06 10:44:54 +0700

    update: ubah ke ssr php

---
+++ const.php
@@ -0,0 +1,62 @@
+<?php
+$products = [
+  [
+    "id" => 1,
+    "name" => "Nocai NC-UV0609PEIII-II",
+    "tagline" => "MESIN UV FLATBED NC-UV0609 PEIII-II (i-1600)",
+    "photo" => "./assets/images/mesin/nocai-nc-uv0609peiii-ii.webp",
+    "short_description" => "NC-UV0609PEIII-II adalah printer UV flatbed profesional yang dirancang untuk industri printing dengan kebutuhan hasil cetak premium pada berbagai material. Cocok untuk perusahaan advertising, manufaktur, dan industri dekorasi yang membutuhkan hasil cetak berkualitas tinggi pada berbagai media.",
+    "link_product" => "https://vicentra.co.id/product/mesin/digital-printing/uv/nocai-nc-uv0609peiii-ii",
+  ],
+  [
+    "id" => 2,
+    "name" => "MJII UV DTF-30",
+    "tagline" => "UV DTF Printer",
+    "photo" => "./assets/images/mesin/mjii-uv-dtf-30.webp",
+    "short_description" => "MJII UV DTF-30 adalah mesin cetak yang menggunakan teknologi UV untuk mencetak pada berbagai jenis bahan seperti kaca, keramik, plastik, logam, dan lainnya. Mesin ini dilengkapi dengan tiga printhead EPSON i1600 dan menggunakan software RIPPrint. Dengan kecepatan cetak hingga 11 m²/jam dan lebar cetak 360 mm, mesin ini cocok untuk aplikasi dalam kemasan, peralatan rumah tangga, dan peralatan industri.",
+    "link_product" => "https://vicentra.co.id/product/mesin/digital-printing/uv/mjii-uv-dtf-30",
+  ],
+  [
+    "id" => 3,
+    "name" => "NEU Jet MPUVRH1804",
+    "tagline" => "MESIN PRINTER UV ROL TO ROL HP 1804",
+    "photo" => "./assets/images/mesin/neu-jet-mpuvrh1804.webp",
+    "short_description" => "NEU Jet MPUVRH1804 adalah mesin printer dengan spesifikasi tinggi yang dilengkapi dengan 4 printhead EPSON i3200 dan berbagai fitur canggih seperti sistem pengisian tinta tekanan negatif, sistem pengeringan dan pemanasan dengan pemanas IR depan, serta sistem pencahayaan LED pada tempat pencetakan. Mesin ini mendukung mode warna CMYK+W+V dan memiliki ukuran printer 3100×700×1450 mm dengan berat bersih 240 kg.",
+    "link_product" => "https://vicentra.co.id/product/mesin/digital-printing/uv/neu-jet-mpuvrh1804",
+  ]
+];
+
+$salesPeople = [
+  [
+    "id" => 1,
+    "name" => "Layanan 24 Jam",
+    "slug" => "layanan_24_jam",
+    "phone" => "6281232548368",
+    "photo" => "https://vicentra.co.id/storage/sales-people/vicentra-24-jam.webp",
+    "additional_sentence" => "Ada pertanyaan atau ingin memesan produk ini?"
+  ],
+  // [
+  //     "id" => 2,
+  //     "name" => "Yaya",
+  //     "slug" => "yaya",
+  //     "phone" => "6285175005535",
+  //     "photo" => "https://vicentra.co.id/storage/sales-people/sales-yaya.webp",
+  //     "additional_sentence" => "Support 24 jam"
+  // ],
+  // [
+  //     "id" => 3,
+  //     "name" => "Alda",
+  //     "slug" => "alda",
+  //     "phone" => "6285183171200",
+  //     "photo" => "https://vicentra.co.id/storage/sales-people/sales-alda.webp",
+  //     "additional_sentence" => "Ada pertanyaan atau ingin memesan produk ini?"
+  // ],
+  // [
+  //     "id" => 4,
+  //     "name" => "Ella",
+  //     "slug" => "ella",
+  //     "phone" => "6285733399974",
+  //     "photo" => "https://vicentra.co.id/storage/sales-people/sales-ella.webp",
+  //     "additional_sentence" => "Ada pertanyaan atau ingin memesan produk ini?"
+  // ],
+];
---
+++ index.php
@@ -0,0 +1,204 @@
+<?php
+require_once('const.php');
+?>
+<!doctype html>
+<html lang="id">
+
+<head>
+  <!-- Metas -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <meta name="author" content="Vicentra Dev" />
+  <meta name="description" content="Vicentra - Spesialis mesin digital printing UV terbaik. Temukan mesin cetak UV berkualitas untuk berbagai kebutuhan, dari flatbed hingga roll to roll. Kami menyediakan mesin baru dan bekas dengan harga terbaik serta layanan purna jual terpercaya. Dapatkan solusi cetak berkualitas tinggi untuk bisnis Anda. Hubungi kami sekarang!" />
+  <meta name="keywords" content="digital printing uv, digital uv printing machine, harga mesin digital printing a3, harga mesin digital printing uv, harga mesin uv printing, jual mesin digital printing uv, mesin digital printing uv, mesin digital printing uv a3, mesin digital printing uv a3 surabaya, mesin digital printing uv a4, mesin digital printing uv a4 surabaya, mesin digital printing uv adalah, mesin digital printing uv akrilik, mesin digital printing uv alpha-jet plus, mesin digital printing uv b, mesin digital printing uv berapa, mesin digital printing uv berapa harganya, mesin digital printing uv black, mesin digital printing uv blend, mesin digital printing uv botol, mesin digital printing uv cetak, mesin digital printing uv cleaner, mesin digital printing uv coating, mesin digital printing uv color, mesin digital printing uv curing, mesin digital printing uv di surabaya, mesin digital printing uv driyorejo, mesin digital printing uv dryer, mesin digital printing uv dtf, mesin digital printing uv e katalog, mesin digital printing uv epson, mesin digital printing uv exposure, mesin digital printing uv filament, mesin digital printing uv filter, mesin digital printing uv filter surabaya, mesin digital printing uv flatbed, mesin digital printing uv flatbed focus alpha-jet plus, mesin digital printing uv flatbed mini, mesin digital printing uv flatbed murah, mesin digital printing uv flatbed riecat uv gen 3, mesin digital printing uv food, mesin digital printing uv foto, mesin digital printing uv gel, mesin digital printing uv gen 3, mesin digital printing uv glossy, mesin digital printing uv gold, mesin digital printing uv grando, mesin digital printing uv green, mesin digital printing uv greenhouse, mesin digital printing uv grid, mesin digital printing uv index, mesin digital printing uv indoor, mesin digital printing uv ink, mesin digital printing uv jakarta, mesin digital printing uv jam tangan, mesin digital printing uv jaman dulu, mesin digital printing uv jet, mesin digital printing uv jogja, mesin digital printing uv jual, mesin digital printing uv jurnal, mesin digital printing uv kediri, mesin digital printing uv kertas, mesin digital printing uv kit, mesin digital printing uv lamp, mesin digital printing uv lampu, mesin digital printing uv laser, mesin digital printing uv led, mesin digital printing uv led flatbed focus alpha-jet plus, mesin digital printing uv light, mesin digital printing uv machine, mesin digital printing uv matte, mesin digital printing uv melior, mesin digital printing uv mesin, mesin digital printing uv mimaki, mesin digital printing uv mini, mesin digital printing uv murah, mesin digital printing uv murah surabaya, mesin digital printing uv nocai, mesin digital printing uv nocai 6040, mesin digital printing uv offset, mesin digital printing uv original, mesin digital printing uv otomatis, mesin digital printing uv percetakan, mesin digital printing uv print, mesin digital printing uv printer, mesin digital printing uv printing, mesin digital printing uv quality, mesin digital printing uv quick, mesin digital printing uv resin, mesin digital printing uv resistant, mesin digital printing uv riecat, mesin digital printing uv riecat uv gen 3, mesin digital printing uv ro, mesin digital printing uv roll to roll, mesin digital printing uv sablon, mesin digital printing uv second, mesin digital printing uv shield, mesin digital printing uv shield surabaya, mesin digital printing uv shopee, mesin digital printing uv shopee surabaya, mesin digital printing uv sinar, mesin digital printing uv spot, mesin digital printing uv surabaya, mesin digital printing uv terbaik, mesin digital printing uv terdekat, mesin digital printing uv to roll, mesin digital printing uv tumbler, mesin digital printing uv uang, mesin digital printing uv uniqlo, mesin digital printing uv untuk apa, mesin digital printing uv uv gen 3, mesin digital printing uv uv400, mesin digital printing uv varnish, mesin digital printing uv vis, mesin digital printing uv water purifier, mesin digital printing uv white, mesin digital printing uv whitening, mesin digital printing uv x1, mesin digital printing uv x2, mesin digital printing uv x3, mesin digital printing uv xenon, mesin digital printing uv xerox, mesin digital printing uv yang bagus, mesin digital printing uv yasir, mesin digital printing uv yellow, mesin digital printing uv zebra, mesin printing uv, mesin uv grando, mesin uv nocai, mesin uv nocai 6040, mesin uv print, mesin uv printer, mesin uv printing, mesin uv printing murah, mesin uv riecat, mesin uv taimes, uv digital printing, uv digital printing machine">
+  <meta name="robots" content="index, follow, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
+
+  <title>Vicentra - MESIN UV</title>
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Vicentra - Mesin UV" />
+  <meta property="og:description"
+    content="Vicentra - Spesialis mesin digital printing UV terbaik. Temukan mesin cetak UV berkualitas untuk berbagai kebutuhan, dari flatbed hingga roll to roll. Kami menyediakan mesin baru dan bekas dengan harga terbaik serta layanan purna jual terpercaya. Dapatkan solusi cetak berkualitas tinggi untuk bisnis Anda. Hubungi kami sekarang!" />
+  <meta property="og:image" content="https://mesinuv.com/assets/images/logo-vicentra-black.webp" />
+  <meta property="og:url" content="https://mesinuv.com" />
+  <meta property="og:site_name" content="Vicentra - Mesin UV" />
+
+  <!-- Twitter -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="Vicentra - Mesin UV" />
+  <meta name="twitter:description"
+    content="Vicentra - Spesialis mesin digital printing UV terbaik. Temukan mesin cetak UV berkualitas untuk berbagai kebutuhan, dari flatbed hingga roll to roll. Kami menyediakan mesin baru dan bekas dengan harga terbaik serta layanan purna jual terpercaya. Dapatkan solusi cetak berkualitas tinggi untuk bisnis Anda. Hubungi kami sekarang!" />
+  <meta name="twitter:image" content="https://mesinuv.com/assets/images/logo-vicentra-black.webp" />
+  <meta name="twitter:site" content="https://mesinuv.com" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://cdn.tailwindcss.com"></script>
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-TB3V6DMQ');
+  </script>
+  <!-- End Google Tag Manager -->
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-H9C3WN2N42"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-H9C3WN2N42');
+  </script>
+</head>
+
+<body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TB3V6DMQ" height="0" width="0"
+      style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <div id="app">
+    <!-- HEADER -->
+    <header class="border-b-2 py-4">
+      <div class="lg:w-[80vw] mx-5 lg:mx-auto flex justify-between items-center relative">
+        <img src="./assets/images/logo-vicentra-black.webp" alt="logo-vicentra-black" class="h-[2.375rem]">
+        <div class="lg:hidden">
+          <i :class="[isMenuShow ? 'fa-solid fa-xmark text-2xl' : 'fa-solid fa-bars text-2xl']" class=""
+            @click="toggleMenu"></i>
+        </div>
+        <ul :class="[isMenuShow ? 'visible' : 'invisible']"
+          class="lg:visible flex flex-col lg:flex-row lg:items-center gap-2 lg:gap-5 absolute lg:static top-[3.125rem] left-0 right-0 bg-white lg:bg-transparent shadow lg:shadow-none p-2 lg:p-0 rounded lg:rounded-none">
+          <li><a href="https://vicentra.co.id" class="beranda hover:underline">Beranda</a></li>
+          <li><a href="https://vicentra.co.id/product/mesin/digital-printing/uv"
+              class="semua_produk hover:underline">Produk</a>
+          </li>
+          <li><a href="https://vicentra.co.id/terms-and-conditions"
+              class="layanan_dan_perbaikan hover:underline">Layanan dan Perbaikan</a>
+          </li>
+          <li><a href="https://vicentra.co.id/about-us" class="tentang_kami hover:underline">Tentang Kami</a></li>
+        </ul>
+      </div>
+    </header>
+    <!-- HEADER -->
+
+    <!-- HERO SECTION -->
+    <section class="lg:w-[80vw] mx-5 lg:mx-auto grid lg:grid-cols-2 items-center mx-5 px-5 py-[3.125rem] bg-[#f7f7f7]">
+      <div>
+        <h1 class="text-4xl font-semibold">
+          MESIN UV TERBAIK, PERIOD
+        </h1>
+        <p class="mt-5">
+          Selamat datang di website kami! Temukan mesin digital printing uv berkualitas dengan hasil tahan lama dan
+          harga terbaik di sini.
+        </p>
+        <a href="https://vicentra.co.id/product/mesin/digital-printing/uv" target="_blank" class=" semua_produk inline-block capitalize mt-5 px-10 py-3 hover:bg-[#1B0000] hover:text-white border border-[#1B0000]
+          transition duration-300">lihat
+          semua produk</a>
+      </div>
+      <div class="order-first lg:order-none">
+        <img src="./assets/images/mesin/nocai-nc-uv0609peiii-ii.webp" alt="mesin-neu-jet">
+      </div>
+    </section>
+    <!-- HERO SECTION -->
+
+    <!-- PRODUCT SECTION -->
+    <section>
+      <div class="lg:w-[80vw] mx-5 lg:mx-auto">
+        <?php foreach ($products as $product) : ?>
+          <div>
+            <div class="flex justify-center mt-10">
+              <div class="w-[80vw] h-[1px] bg-[#1B0000]"></div>
+            </div>
+            <div class="grid lg:grid-cols-2 gap-5 items-center my-[5rem]">
+              <div>
+                <h2><?= $product['name'] ?></h2>
+                <h1 class="text-3xl font-semibold my-5"><?= $product['tagline'] ?></h1>
+                <div>
+                  <?= $product['short_description'] ?>
+                </div>
+                <a href="<?= $product['link_product'] ?>" target="_blank"
+                  class="detail_produk inline-block capitalize font-medium text-white bg-black rounded-md px-10 py-3 mt-5">lebih
+                  lengkap</a>
+              </div>
+              <div class="order-first lg:order-none">
+                <a href="<?= $product['link_product'] ?>" target="_blank">
+                  <img src="<?= $product['photo'] ?>" alt="<?= $product['name'] ?>">
+                </a>
+              </div>
+            </div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    </section>
+    <!-- PRODUCT SECTION -->
+
+    <!-- CTA SECTION -->
+    <section class="mb-[2.5rem]">
+      <h1 class="capitalize text-3xl font-semibold text-center">Hubungi Kami</h1>
+      <div class="relative flex justify-center mt-5">
+        <div class="w-[80vw] h-[2px] bg-[#e1e1e1]"></div>
+        <div class="w-[10vw] h-[2px] bg-[#1B0000] absolute"></div>
+      </div>
+      <div class="lg:w-[40vw] mx-5 lg:mx-auto flex justify-center mt-10">
+        <?php foreach ($salesPeople as $sales): ?>
+          <a href="https://api.whatsapp.com/send?phone=<?= $sales['phone']; ?>&text=Hallo%20Vicentra,%20Saya%20Ingin%20Memesan%20Mesin%20UV."
+            target="_blank"
+            class="<?= ($sales['id'] === 1) ? 'bg-[#006db8]' : 'bg-green-600'; ?> sales_<?= $sales['slug']; ?>_cta_link w-full h-[5rem] flex items-center gap-[1.5rem] rounded-full relative py-2 pl-[7rem]">
+
+            <div class="absolute left-0">
+              <div class="w-[5.5rem] h-[5.5rem] rounded-full border-[0.188rem] border-white relative">
+                <img src="<?= $sales['photo']; ?>" alt="<?= $sales['name']; ?>" class="rounded-full" />
+                <div class="w-[1.5rem] h-[1.5rem] flex justify-center items-center bg-white rounded-full absolute right-[-0.75rem] top-[50%] transform translate-y-[-50%]">
+                  <i class="fa-brands fa-whatsapp text-green-500"></i>
+                </div>
+              </div>
+            </div>
+
+            <div class="mr-4">
+              <div class="flex gap-2">
+                <h1 class="text-sm font-medium text-white/50 capitalize">
+                  <?= $sales['name']; ?>
+                </h1>
+                <span class="flex justify-center items-center text-xs font-semibold text-white bg-white/20 px-2 rounded-md capitalize">
+                  online
+                </span>
+              </div>
+              <p class="w-full text-sm font-medium text-white mt-1">
+                <?= $sales['additional_sentence']; ?>
+              </p>
+            </div>
+
+          </a>
+        <?php endforeach; ?>
+      </div>
+    </section>
+    <!-- CTA SECTION -->
+
+    <!-- FOOTER -->
+    <footer class="bg-black py-5 flex justify-center items-center">
+      <p class="text-sm text-white">Vicentra © 2025 Created By Vicentra Dev.</p>
+    </footer>
+    <!-- FOOTER -->
+  </div>
+
+  <script defer type="module" src="./assets/js/app.js"></script>
+</body>
+
+</html>
